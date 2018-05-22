6d0abe46f8b2c239c6f5be7d71cf0e8efff13ede
bartbc <cbarthes11@gmail.com>
2018-05-22 09:55:14 +0200

    modif au 21/05

--- JS/script.js
+++ JS/script.js
@@ -17,7 +17,7 @@ window.onload = function (event) {
         break;
     }
     zoneSelect.value='start';//retour position par défaut
-    console.log(event);
+    console.log(event);//lit les évents !
   });
           
   function nbContacts() {
@@ -27,11 +27,22 @@ window.onload = function (event) {
     alert('listContacts !');
   };
   function newContact() {
-    alert('newContact !');
+    //création formulaire
+    var form ='<form id="creatContact">';//ouverture formulaire
+    form+='<div><label for="name">Nom : </label><input type="text" id="name"></div>';
+    form+='<div><label for="firstName">Prénom : </label><input type="text" id="firstName"></div>';
+    form+='<div><label for="phoneNumber">N° tél. : </label><input type="text" id="phoneNumber"></div>';
+    form+='<div><input type="button" value="Go" id="button" title="valider" /></div>';
+    form+='</form>';//fin du form
+    //création de la classe du formulaire
+    
+    var zoneAffichage=document.getElementsByClassName('zoneAffichage')[0];
+    zoneAffichage.innerHTML=form;
   };
   
 
   function creatContact() {
+
       //recupère les données saisies
 
       //controle validité saisie (format numéro) // voir si possible masque de saisie
