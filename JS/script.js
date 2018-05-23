6dcf8ca4acdb29b8d81101abc285fe3a85c26447
bartbc <cbarthes11@gmail.com>
2018-05-23 09:24:36 +0200

    mis à jour au 23/05/2018 9:00

--- JS/script.js
+++ JS/script.js
@@ -1,80 +1,72 @@
-
 //init tableau 
 tableau = [];
 
+//pour tester!!! XXXxxxXXXXxx
+tableau.push([["Texiera;Marine;0611223344"]]);
+tableau.push([["Barthès;Christophe;0755667788"]]);
+
 function newContact() {
-//création formulaire
-var form ='<form id="creatContact">';//ouverture formulaire
-form+='<div><label for="name">Nom : </label><input type="text" id="name"></div>';
-form+='<div><label for="firstName">Prénom : </label><input type="text" id="firstName"></div>';
-form+='<div><label for="phoneNumber">N° tél. : </label><input type="text" id="phoneNumber"></div>';
-form+='<div><input type="submit" value="Go" id="button" title="valider" onclick="return false;"/></div>';
-form+='</form>';//fin du form
-//création de la classe du formulaire
-var zoneAffichage=document.getElementsByClassName('zoneAffichage')[0];
-zoneAffichage.innerHTML=form;
-//ajout de l'eventlistenner submit
-// Accédez à l'élément form …
-var ele = document.getElementById("button");
-// … et prenez en charge l'événement submit.
-ele.addEventListener("click", function (event) { // () => {
-  // event.preventDefault();
-  // sendData(); 
-  var name=document.forms["creatContact"].elements["name"].value
-  var firstName=document.forms["creatContact"].elements["firstName"].value
-  var phoneNumber=document.forms["creatContact"].elements["phoneNumber"].value
-  creatContact(name,firstName,phoneNumber);
+  //création formulaire
+  var form ='<form id="creatContact">';//ouverture formulaire
+  form+='<div><label for="name">Nom : </label><input type="text" id="name" autofocus></div>';
+  form+='<div><label for="firstName">Prénom : </label><input type="text" id="firstName"></div>';
+  form+='<div><label for="phoneNumber">N° tél. : </label><input type="tel" id="phoneNumber"></div>';
+  form+='<div><input type="button" value="Go" id="button" title="valider" /></div>';
+  form+='</form>';//fin du form
+  // ajout du form dans le html
+  var zoneAffichage=document.getElementsByClassName('zoneAffichage')[0];
+  zoneAffichage.innerHTML=form;
+  // Accédez à l'élément button et ajout de l'eventlistenner click
+  var ele = document.getElementById("button");
+  ele.addEventListener("click", function (event) { // () => {
+    //récupération
+    var name=document.forms["creatContact"].elements["name"].value
+    var firstName=document.forms["creatContact"].elements["firstName"].value
+    var phoneNumber=document.forms["creatContact"].elements["phoneNumber"].value
+    //controle des sasisies
+    if ((name==="" && firstName==="")) {
+      alert('Le nom ou le prénom doit être renseigné');
+    } else {
+    creatContact(name,firstName,phoneNumber);
+    }
   });
 }
 
+function listContacts() {
+  var tabl=tableau;
+  var sep=';';
+  var chtmeul='<h2>Liste de vos contacts</h2><ul>';
+    for (i=0; i<tabl.length; i++) {
+      var list=tabl[i].toString(); //force à transformer en string 
+      var monArray = list.split(sep);
+      var name=monArray[0];
+      var firstName=monArray[1];
+      var phoneNumber=monArray[2];
+      chtmeul+='<li class="enteteContacts"> - '+name+' '+firstName+'</li><li class="separContacts">  '+phoneNumber+'</li>';//ajout li pour chaque contact
+    }
+  chtmeul+='</ul>'
+  var zoneAffichage=document.getElementsByClassName('zoneAffichage')[0];
+  zoneAffichage.innerHTML=chtmeul;
+};
+
 function creatContact(name,firstName,phoneNumber) {
-  // alert('longueur tableaux '+tableau.length);
-  tableau.push([[name,firstName,phoneNumber]]);
-  // alert('longueur tableaux '+tableau.length);
-  //efface contenu form
-  document.getElementById("creatContact").reset();
-  // alert(tableau);
-    // alert(name + ' / ' + firstName +' / '+ phoneNumber);
+  tableau.push([[name+";"+firstName+";"+phoneNumber]]);
+  document.getElementById("creatContact").reset();//reset les values du form
+  document.getElementById("name").focus();//focus
 }
 
 function nbContacts() {
-  // alert('nbContacts : '+ tableau.length);
   var nbContacts=tableau.length;
   var chtmeul='<h2>Vous avez '+nbContacts+' contacts</h2>';
   var zoneAffichage=document.getElementsByClassName('zoneAffichage')[0];
   zoneAffichage.innerHTML=chtmeul;
 };
 
-function listContacts() {
-var tabl=tableau;
-var sep=',';
-for (i=0; i<tableau.length; i++) {
-  alert(tabl[i]);
-
-
-  var arrayOfStrings = tabl[i].split(sep);
-
-  for (var i=0; i < arrayOfStrings.length; i++)
-    alert(arrayOfStrings[i] + " / ");
-}
-
-
-
-  
-};
-
-
-
-
-
-
-
+//optimiser en créant une fonction pour le html généré ????
 
 window.onload = function (event) {
   var zoneSelect= document.getElementsByClassName('selector')[0];//pour l'ID  document.getElementById('selector')
-
   // zoneSelect.value='start';//se position par défaut sur start    
-
   zoneSelect.addEventListener('change', () => {// ajout event 'change' sur select
     switch (zoneSelect.value) {
       case 'nbContacts':
@@ -87,8 +79,6 @@ window.onload = function (event) {
       newContact();
         break;
     }
-    // zoneSelect.value='start';//se position par défaut sur start
-    console.log(event);//lit les évents !// a supprimer tableau.length
   });        
 };
 
